eb915f212b3650f4f3e8f656f4369423b228e465
Salvador Cabeza <scabeza@outlook.com>
2025-11-25 16:59:10 -0500

    modificacion app.js

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-
-
 function main() {
 
-    let 
+    const userForm = document.getElementById("user-form");
+    let userTable = document.getElementById("user-table-body");
 
 
+    function createUsers() {
+        let userName = document.getElementById("userName").value;
+        let email = document.getElementById("email").value;
+        let age = document.getElementById("age").value;
+        let status = document.getElementById("status").value;
+        userTable.innerText =`${userName} y status ${status}`;
+    }
 
+    userForm.addEventListener('submit', ()=> createUsers());
 
 }
 
