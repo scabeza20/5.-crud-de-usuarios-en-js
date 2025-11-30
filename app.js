38ad6a91d3746919c9a0454567813ce2a4c98bf5
Salvador Cabeza <scabeza@outlook.com>
2025-11-29 22:51:04 -0500

    borre el console.log

--- app.js
+++ app.js
@@ -10,8 +10,7 @@ function main() {
         let email = document.getElementById("email").value;
         let age = document.getElementById("age").value;
         let status = document.getElementById("status").value;
-        userTable.innerHTML +=`<tr> <td>${userName}</td> <td>${email}</td> <td>${age}</td> <td>${status}</td> <td><button>Actualizar</button></td> <td><button>Eliminar</button></td> </tr>  `;
-        console.log(userTable.[0]);
+        userTable.innerHTML +=`<tr> <td>${userName}</td> <td>${email}</td> <td>${age}</td> <td>${status}</td> <td><button>Actualizar</button></td> <td><button>Eliminar</button></td> </tr> `;
     }
 
     userForm.addEventListener('submit', createUser);
