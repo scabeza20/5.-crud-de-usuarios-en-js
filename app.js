854784d2714b18dc78a202ffe2d22dc45fd43f85
Salvador Cabeza <scabeza@outlook.com>
2025-11-30 21:13:37 -0500

    finish crud with update and delete

--- app.js
+++ app.js
@@ -3,17 +3,31 @@ function main() {
     const userForm = document.getElementById("user-form");
     let userTable = document.getElementById("user-table-body");
 
-
-    function createUser(event) {
+     function createUser(event) {
         event.preventDefault();
         let userName = document.getElementById("userName").value;
         let email = document.getElementById("email").value;
         let age = document.getElementById("age").value;
         let status = document.getElementById("status").value;
-        userTable.innerHTML +=`<tr> <td>${userName}</td> <td>${email}</td> <td>${age}</td> <td>${status}</td> <td><button>Actualizar</button></td> <td><button>Eliminar</button></td> </tr> `;
+        userTable.innerHTML +=`<tr> <td>${userName}</td> <td>${email}</td> <td>${age}</td> <td>${status}</td> <td><button class="btn-update">Actualizar</button></td> <td><button class="btn-delete">Eliminar</button></td> </tr> `;
+
     }
 
     userForm.addEventListener('submit', createUser);
+
+    userTable.addEventListener("click", (e) => {
+        if (e.target.classList.contains("btn-delete")) {
+            e.target.closest("tr").remove();
+        }
+
+        if (e.target.classList.contains("btn-update")) {
+            let userName = document.getElementById("userName").value;
+            let email = document.getElementById("email").value;
+            let age = document.getElementById("age").value;
+            let status = document.getElementById("status").value;
+            e.target.closest("tr").innerHTML=`<tr> <td>${userName}</td> <td>${email}</td> <td>${age}</td> <td>${status}</td> <td><button class="btn-update">Actualizar</button></td> <td><button class="btn-delete">Eliminar</button></td> </tr> `;
+        }
+    });
 
 }
 
